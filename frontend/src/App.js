cfe8c68d6316342e76284ad886bb4d69de1d96cc
MaximeGillot5 <mgillot56@icloud.com>
2023-06-19 13:45:35 +0200

    login page and jwt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,25 @@ import Home from './page/Home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import About from './page/About';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import LoginForm from "./page/LoginForm"
+
 
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Home />
-      {/* <About /> */}
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<LoginForm />} />
+          </Routes>
+        </div>
+      </Router>
+
       <Footer />
     </div>
   );
