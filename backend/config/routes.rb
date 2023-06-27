863c924e6f38b0e07c530cdfcd0439bcc8d81922
MaximeGillot5 <mgillot56@icloud.com>
2023-06-27 15:37:45 +0200

    desploy test

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'users#index'
+
   get 'current_user', to: 'current_user#index'
   resources :users, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   resources :sports, only: [:index, :show, :new, :create, :edit, :update, :destroy]
